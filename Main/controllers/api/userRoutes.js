7a107dd3de09fd67c343ea516fa5233f7c865444
Jackson <jacksonfarren@hotmail.com>
2022-11-21 20:22:25 -0800

    Base userRoutes api

--- Main/controllers/api/userRoutes.js
+++ Main/controllers/api/userRoutes.js
@@ -1,4 +1,128 @@
 // Imports
 const router = require("express").Router();
+const { User } = require('../../models')
 
 // User Routes
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll({
+            attributes: {
+                exclude: ['password']
+            }
+        })
+
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// route for one User with included model relationships 
+
+/* 
+    router.get('/:id', async(req, res) => {
+        const userData = User.findOne({
+            attributes: {
+                exclude: ['password']
+            }, 
+
+            where: { id: req.params.id },
+
+            include: [{
+                (models)
+                (attributes)
+            }]
+        })
+    })
+*/
+
+router.post("/login", async (req, res) => {
+    try {
+        const userData = await User.findOne(
+            {
+                where: { username: req.body.username }
+            }
+        )
+
+        if (!userData) {
+            res.status(400).json({ message: "Invalid username. Please try again." })
+        }
+
+        const validPassword = userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Invalid password. Please try again' })
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.logged_in = true;
+
+            res.status(200).json({ message: 'You are now logged in.' })
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post("/logout", async (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+router.post('/signup', async (req, res) => {
+    try {
+        const userData = User.create({
+            username: req.body.username,
+            password: req.body.password
+        })
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const userData = await User.update(req.body, {
+            where: { id: req.params.id }
+        });
+
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with this id.' })
+        }
+
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const userData = await User.destroy({
+            where: { id: req.params.id }
+        })
+
+        if(!userData) {
+            res.status(404).json({ message: 'No user found with this id' });
+        }
+
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})

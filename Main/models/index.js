0388ff9b1989979448281bea8cdee5785db02373
JamesJPorter <porterjjames@gmail.com>
2022-11-23 14:51:49 -0800

    fsdfgsd

--- Main/models/index.js
+++ Main/models/index.js
@@ -4,49 +4,84 @@ const Division = require('./Division');
 const Project = require('./Project');
 const Employee = require('./Employee');
 const Permission = require('./Permissions');
+const EmployeeAssignments = require('./EmployeeAssignments');
 
-Organization.hasMany(Division, {
+User.hasOne(Permissions, {
+    foreignKey: "permission_id", 
+    onDelete: null
+});
+
+Employee.hasOne(User, {
+    foreignKey: "user_ud", 
+    onDelete: "CASCADE"
+});
+
+Employee.hasMany(EmployeeAssignments, {
+    foreignKey: "project_id", 
+    foreignKey: "project_id",
+    onDelete: null
+});
+
+Employee.hasOne(Division, {
     foreignKey: "division_id", 
-    onDelete: 'CASCADE'
-});
-
-Division.hasOne(Organization, {
-    foreignKey: "organization_id", 
-});
-
-Division.hasOne(Organization, {
-    foreignKey: "organization_id", 
-});
-
-Organization.hasMany(Employee, {
-    foreignKey: "organization_id", 
+    onDelete: null
 });
 
 Employee.hasOne(Organization, {
-    foreignKey: "organization_id",
+    foreignKey: "organization_id", 
+    onDelete: null
 });
 
-Division.hasMany(Project, {
-    foreignKey: "project_id", 
-});
+Employee.
 
-Project.hasOne(Division, {
-    foreignKey: 'division_id'
-});
 
-Project.hasMany(Employee, {
-    foreignKey: "employee_id"
-});
 
-Employee.hasMany(Project, {
-    foreignKey: "project_id"
-});
 
-Employee.hasOne(Permission, {
-    foreignKey: "permission_id"
-});
 
-Permission.hasMany(Employee, {
-    foreignKey: "employee_id"
-});
+
+
+// Organization.hasMany(Division, {
+//     foreignKey: "division_id", 
+//     onDelete: 'CASCADE'
+// });
+
+// Division.hasOne(Organization, {
+//     foreignKey: "organization_id", 
+// });
+
+// Division.hasOne(Organization, {
+//     foreignKey: "organization_id", 
+// });
+
+// Organization.hasMany(Employee, {
+//     foreignKey: "organization_id", 
+// });
+
+// Employee.hasOne(Organization, {
+//     foreignKey: "organization_id",
+// });
+
+// Division.hasMany(Project, {
+//     foreignKey: "project_id", 
+// });
+
+// Project.hasOne(Division, {
+//     foreignKey: 'division_id'
+// });
+
+// Project.hasMany(Employee, {
+//     foreignKey: "employee_id"
+// });
+
+// Employee.hasMany(Project, {
+//     foreignKey: "project_id"
+// });
+
+// Employee.hasOne(Permission, {
+//     foreignKey: "permission_id"
+// });
+
+// Permission.hasMany(Employee, {
+//     foreignKey: "employee_id"
+// });
 

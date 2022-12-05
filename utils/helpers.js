cbbf687847d67b47e60304757b18b3593c5f1f3a
Jackson <jacksonfarren@hotmail.com>
2022-12-05 03:26:02 -0800

    Fixed available employees with helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,14 +6,23 @@ module.exports = {
   },
   permissionCheck: function (value) {
     if (value === 1) {
-      const permissionValidated = false;
+      const permissionValidated = true;
       return permissionValidated;
     } else {
-      const permissionValidated = true;
+      const permissionValidated = false;
       return permissionValidated;
     }
   },
   parseIndex: (val) => {
     return parseInt(val) + 1;
   },
+  hasProject: (val) => {
+    if (val >= 1) {
+      const available = false
+      return available;
+    } else {
+      const available = true;
+      return available;
+    }
+  }
 };
